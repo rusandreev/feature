4bb73d30c19c163f8dc35dbc8a5b38612e455ef4
Ruslan Andreev <rus.andreev@gmail.com>
2022-06-24 19:50:15 +0300

    Objects annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -21,3 +21,21 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 }
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+// const logWeather = (forecast: {date: Date, weather: string}): void => {
+//   console.log(forecast.date);
+//   console.log(forecast.weather);
+// };
+
+// ES2015
+const logWeather = ({date, weather}: {date: Date, weather: string}) => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
